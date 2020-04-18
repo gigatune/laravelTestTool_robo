b68459cbf1f0f62854d98d62d1797f630940ca11
gigatune <gigatune@users.noreply.github.com>
2020-04-18 17:19:42 +0900

    受け入れテストの設定を変更

--- RoboFile.php
+++ RoboFile.php
@@ -2,19 +2,35 @@
 
 class RoboFile extends \Robo\Tasks
 {
+    var $single_acceptancefile = null;
+
     public function watch(){
         $this->taskWatch()
             ->monitor(
-                '../codeception/tests/acceptance',
+                [ '../codeception/tests/acceptance',
+                  '../resources/views',
+                  '../app/',
+                  '../app/Http/Controllers',
+                ],
                 function( $event ) {
                     $resource = $event->getResource();
-                    $this->acceptance_single( $resource );
+                    if( preg_match('~acceptance~', $resource ) ){
+                        $this->change_acceptancefile($resource);
+                    }
+                    $this->acceptance_single();
                 }
             )->run();
     }
+    public function change_acceptancefile( $file = null ){
+        $this->single_acceptancefile = $file;
+    }
 
-    public function acceptance_single( $file = null ){
-        $this->_exec( '../vendor/bin/codecept --config=../codeception/codeception.yml run acceptance ' . $file );
+    public function acceptance_single( ){
+        if( $this->single_acceptancefile == null ){
+            $this->say('受け入れテストが指定されていません');
+            return;
+        }
+        $this->_exec( '../vendor/bin/codecept --config=../codeception/codeception.yml run acceptance ' . $this->single_acceptancefile );
     }
 }
 ?>
